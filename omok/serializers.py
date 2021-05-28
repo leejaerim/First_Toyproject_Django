3f1baddb7519ad778593fc4942c12d2d2bfb4d1a
leejaerim <ger6608@naver.com>
2021-05-28 15:19:47 +0900

    POST/PUT fixed

--- omok/serializers.py
+++ omok/serializers.py
@@ -13,4 +13,5 @@ class UserSerializer(serializers.ModelSerializer):
 class RoomSerializer(serializers.ModelSerializer):
        class Meta:
             model = Room
-            fields = ['id','title', 'user', 'isAvailable', 'hasPassword']
+            fields = ['id','title','room_password', 'user', 'isAvailable', 'hasPassword']
+            extra_kwargs = {'room_password': {'write_only': True, 'min_length': 4}}
