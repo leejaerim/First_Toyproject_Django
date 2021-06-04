bc8487c16bcdf42a37e600738cf0480326bb4c90
leejaerim <ger6608@naver.com>
2021-06-04 13:42:01 +0000

    unfinished omok_graphene

--- mysite/schema.py
+++ mysite/schema.py
@@ -3,15 +3,37 @@ import graphene
 from graphene_django import DjangoObjectType
 from graphene.types.argument import Argument
 from todolist.models import Todo
-
+from omok.models import User, Room
 class TodoType(DjangoObjectType):
     class Meta:
         model = Todo
         # field = ("id","text","isCompleted")
+class RoomType(DjangoObjectType):
+    class Meta:
+        model = Room
+        # field = ("id","title","hasPassword","isAvailable")
+        exclude_fields = [
+            'password', #it worked          
+        ]
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
 
 class Query(graphene.ObjectType):
     todos = graphene.List(TodoType)
     todo = graphene.Field(TodoType, id = graphene.ID())
+    users = graphene.List(UserType)
+    user = graphene.Field(UserType, id = graphene.ID())
+    rooms = graphene.List(RoomType)
+    room = graphene.Field(RoomType, id = graphene.ID())
+    def resolve_users(self, info, **kwargs):
+        return User.objects.all()
+    def resolve_user(self, info, id):
+        return User.objects.get(pk=id)
+    def resolve_rooms(self, info, **kwargs):
+        return Room.objects.all()
+    def resolve_room(self, info, id):
+        return Room.objects.get(pk=id)
     def resolve_todos(self, info, **kwargs):
         # We can easily optimize query count in the resolve method
         return Todo.objects.all()
@@ -32,6 +54,25 @@ class CreateTodo(graphene.Mutation):
         return CreateTodo(
         todo = todo 
         )
+class CreateUser(graphene.Mutation):
+    class Arguments:
+        name = graphene.String()
+    user = graphene.Field(UserType)
+    def mutate(self, info,name):
+        user = User.objects.create(
+            name = name
+        )
+        user.save()
+        return CreateUser(
+        user = user
+        )
+class CreateRoom(graphene.Mutation):
+    class Arguments:
+        title = graphene.String()
+        password = graphene.String()
+        isAvailable = graphene.Int()
+        hasPassword = graphene.Int()
+        user = graphene.field(UserType)
 class UpdateTodo(graphene.Mutation):
     class Arguments:
         id = graphene.ID()
@@ -59,5 +100,6 @@ class Mutation(graphene.ObjectType):
     create_todo = CreateTodo.Field()
     update_todo = UpdateTodo.Field()
     delete_todo = DeleteTodo.Field()
+    create_user = CreateUser.Field()
 
 schema = graphene.Schema(query=Query, mutation= Mutation)
