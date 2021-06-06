e6d8771a2869c5b417c2f0b8d09eb5ce92e11540
hyobbb <yang4h0b1>
2021-06-06 13:26:55 +0000

    omok graphql migration

--- mysite/schema.py
+++ mysite/schema.py
@@ -1,29 +1,9 @@
 import graphene
 from graphene_django.debug import DjangoDebug
-from graphene_django import DjangoObjectType
 from todolist.models import Todo
+from todolist.schema import TodoType, CreateTodo, UpdateTodo, DeleteTodo
 from omok.models import User, Room
-
-
-class TodoType(DjangoObjectType):
-    class Meta:
-        model = Todo
-        field = ("id", "text", "isCompleted")
-
-
-class RoomType(DjangoObjectType):
-    class Meta:
-        model = Room
-        field = ("id", "title", "hasPassword", "isAvailable")
-        exclude_fields = [
-            'password',  #it worked          
-        ]
-
-
-class UserType(DjangoObjectType):
-    class Meta:
-        model = User
-        field = ("id", "name")
+from omok.schema import UserType, RoomType, CreateRoom, DeleteRoom
 
 
 class Query(graphene.ObjectType):
@@ -54,70 +34,14 @@ class Query(graphene.ObjectType):
         return Todo.objects.get(pk=id)
 
 
-class CreateTodo(graphene.Mutation):
-    class Arguments:
-        text = graphene.String()
-        isCompleted = graphene.Boolean(required=False)
-
-    id = graphene.ID()
-    text = graphene.String()
-    isCompleted = graphene.Boolean()
-
-    def mutate(self, info, text, isCompleted=None):
-        _isCompleted = None
-        if isCompleted is not None:
-            if isCompleted:
-                _isCompleted = 1
-            else :
-                _isCompleted = 0
-        todo = Todo.objects.create(text=text, isCompleted=_isCompleted)
-        todo.save()
-        return CreateTodo(id=todo.id, text= text, isCompleted=isCompleted)
-
-
-class UpdateTodo(graphene.Mutation):
-    class Arguments:
-        id = graphene.ID()
-        text = graphene.String()
-        isCompleted = graphene.Boolean()
-
-    id = graphene.ID()
-    text = graphene.String()
-    isCompleted = graphene.Boolean()
-
-    def mutate(self, info, id, text=None, isCompleted=None):
-        _isCompleted = None
-        if isCompleted is not None:
-            if isCompleted:
-                _isCompleted = 1
-            else :
-                _isCompleted = 0
-        
-        todo = Todo.objects.get(pk=id)
-        todo.text = text if text is not None else todo.text
-        todo.isCompleted = _isCompleted
-        todo.save()
-        return UpdateTodo(id=id, text = text, isCompleted=isCompleted)
-
-
-class DeleteTodo(graphene.Mutation):
-    class Arguments:
-        id = graphene.ID()
-
-    id = graphene.ID()
-
-    def mutate(self, info, id):
-        todo = Todo.objects.get(pk=id)
-        if todo is not None:
-            todo.delete()
-        return DeleteTodo(id=id)
-
-
 
 class Mutation(graphene.ObjectType):
     create_todo = CreateTodo.Field()
     update_todo = UpdateTodo.Field()
     delete_todo = DeleteTodo.Field()
+    
+    create_room = CreateRoom.Field()
+    delete_room = DeleteRoom.Field()
     debug = graphene.Field(DjangoDebug, name='_debug')
 
 
---
+++ omok/schema.py
@@ -0,0 +1,104 @@
+import graphene
+from graphene_django import DjangoObjectType
+from .models import User, Room
+
+
+class RoomType(DjangoObjectType):
+    class Meta:
+        model = Room
+        field = ("id", "title", "hasPassword", "isAvailable")
+        exclude_fields = [
+            'password',  #it worked          
+        ]
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+        field = ("id", "name")
+
+
+class UserInput(graphene.InputObjectType):
+    id = graphene.ID(required=True)
+    name = graphene.String(required=True)
+    
+
+class CreateUser(graphene.Mutation):
+    class Arguments:
+        name = graphene.String()
+
+    id = graphene.ID()
+    name = graphene.String()
+
+    def mutate(self, info, name):
+        user = User.objects.create(name=name)
+        user.save()
+        return CreateUser(id=user.id, name=name)
+
+
+class CreateRoom(graphene.Mutation):
+    class Arguments:
+        title = graphene.String()
+        password = graphene.String()
+        user = graphene.Argument(UserInput)
+
+    id = graphene.ID()
+    title = graphene.String()
+    isAvailable = graphene.Boolean()
+    hasPassword = graphene.Boolean()
+    user = graphene.Field(UserType)
+
+    def mutate(self, info, title, password, user, isAvailable=True):
+        _hasPassword = 0 if password is None else 1
+        _user = User(id=user.id, name = user.name)
+        room = Room.objects.create(title=title,
+                                   password=password,
+                                   isAvailable=isAvailable,
+                                   hasPassword=_hasPassword,
+                                   user = _user)
+        return CreateRoom(id=room.id,
+                          title=title,
+                          isAvailable=isAvailable,
+                          hasPassword=False if password is None else True,
+                          user = _user)
+
+
+class UpdateUser(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        name = graphene.String()
+
+    id = graphene.ID()
+    name = graphene.String()
+
+    def mutate(self, info, id, name):
+        user = User.objects.get(pk=id)
+        user.name = name
+        return UpdateUser(id=id, name=name)
+
+
+class DeleteUser(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+
+    id = graphene.ID()
+
+    def mutate(self, info, id):
+        user = User.objects.get(pk=id)
+        if user is not None:
+            user.delete()
+        return DeleteUser(id=id)
+
+
+class DeleteRoom(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        #user = graphene.Argument(UserInput)
+
+    id = graphene.ID()
+
+    def mutate(self, info, id):
+        room = Room.objects.get(pk=id)
+        if room is not None:
+            room.delete()
+        return DeleteRoom(id=id)
